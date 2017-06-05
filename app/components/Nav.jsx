113bf51d6b04649031ed24fc21cb91261f02a155
maxkram <max.kramer@live.com>
2017-06-05 17:26:07 +0300

    changed a little

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -5,12 +5,12 @@ var Nav = React.createClass({
   render: function () {
     return (
       <div>
-        <h2>Nav Component</h2>
+        <h2>Навигационный модуль</h2>
         <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Узнать о погоде</IndexLink>
         <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>О программе</Link>
         <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Примеры</Link>
 
-        <a href='#/about'>Go to About</a>
+        <a href='#/about'>В раздел "Подробнее"</a>
       </div>
     );
   }
