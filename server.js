c15278bf5a358995bb5ad46dd5081eb055d9e1eb
maxkram <max.kramer@live.com>
2017-06-05 18:56:47 +0300

    4 heroku

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next){
-  if(req.headers['x-forwarded-proto'] === 'http'){
+  if (req.headers['x-forwarded-proto'] === 'http') {
     next();
-  }else{
+  } else {
     res.redirect('http://' + req.hostname + req.url);
   }
 });
@@ -15,5 +15,5 @@ app.use(function (req, res, next){
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
-  console.log('Express server работает на порту ' + PORT);
+  console.log('Express server is up on port ' + PORT);
 });
